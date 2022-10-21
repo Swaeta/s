06ee29310c8ab205817b1a07be2d107f1dc9482a
Swaeta <66239103+Swaeta@users.noreply.github.com>
2022-10-21 09:23:47 +0530

    Add files via upload

---
+++ sym/pop.js
@@ -0,0 +1,34 @@
+const serviceItems = document.querySelector(".service-items");
+const popup = document.querySelector(".popup-box")
+const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+
+serviceItems.addEventListener("click",function(event){
+    if(event.target.tagName.toLowerCase() == "button"){
+       const item =event.target.parentElement;
+       const h3 = item.querySelector("h3").innerHTML;
+       const viewMoreCont = item.querySelector(".view-more-cont").innerHTML;
+       popup.querySelector("h3").innerHTML = h3;
+       popup.querySelector(".popup-body").innerHTML = viewMoreCont;
+       popupBox();
+
+    }
+
+})
+
+popupCloseBtn.addEventListener("click", popupBox);
+popupCloseIcon.addEventListener("click", popupBox); 
+
+popup.addEventListener("click", function(event){
+    if(event.target == popup){
+        popupBox();
+
+    }
+
+
+})
+
+
+function popupBox(){
+    popup.classList.toggle("open");
+}
